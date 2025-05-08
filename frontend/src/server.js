c681a83dfbe3c2dfbd76fb555a7d348835e96a29
Shivanshu0404 <159226260+Shivanshu0404@users.noreply.github.com>
2025-05-09 00:53:44 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url="http://localhost:8000/";
+export const server = "https://eshop-back-y3bh.onrender.com/api/v2";
+export const backend_url="https://eshop-back-y3bh.onrender.com";
 
 

87b449e1a5ceab979eaa4657ef0a55f11b13b1c6
Shivanshu0404 <159226260+Shivanshu0404@users.noreply.github.com>
2025-05-09 01:19:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://eshop-yq6l.orender.com/',],
+  origin: ['https://eshop-yq6l.orender.com',],
   credentials: true
 }));
 

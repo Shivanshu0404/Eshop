52d7e875ac7591a4e9db921208490517ea3c1611
Shivanshu0404 <159226260+Shivanshu0404@users.noreply.github.com>
2025-05-09 01:45:48 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://eshop-back-qddp.onrender.com/api/v2";
-export const backend_url="https://eshop-back-qddp.onrender.com";
+export const server = "https://eshop-back-4f9s.onrender.com/api/v2";
+export const backend_url="https://eshop-back-4f9s.onrender.com";
 
 
